eff0fbf5e06571b5f694d299752066f2e5e33776
wldhrqnf12 <84691354+wldhrqnf12@users.noreply.github.com>
2021-06-28 16:16:16 +0900

    node js study

---
+++ 6.28 node js/syntax/Template.js
@@ -0,0 +1,4 @@
+// 문법 Template Literal
+var name ='hi'
+var letter = 'Dear' '+name+' blade '+name+' dfhaidhf '+name+' isddhf '+name+' idhfdifhdifh '+name;
+console.log(letter);
---
+++ 6.28 node js/syntax/string.js
@@ -0,0 +1,15 @@
+// 
+console.log('1'+'1');
+// 언어 개수
+console.log('hi blade'.length);
+// 변수의 형식 ex 변수를 넣을 때 var 
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+// 변수의 활용 (중복 제거)
+var name ='hi'
+var letter = 'Dear' +name+' blade '+name+' dfhaidhf '+name+' isddhf '+name+' idhfdifhdifh '+name;
+console.log(letter);
+// 문법 Template Literal
+
